661daeb5640218f055c277218aabdc9afa1531cf
Sergey Radionov <RSATom@gmail.com>
2011-11-10 19:56:48 +0700

    vc-libvlc Initial create.

---
+++ MakeLibVlcLib.bat
@@ -0,0 +1,27 @@
+@echo off
+for %%i in ("%~0") do set THIS_BAT_PATH=%%~dpi
+rem cd "%THIS_BAT_PATH%"
+for %%i in (libvlc.dll) do set LIBVLC_DLL="%%~$PATH:i"
+
+if not exist %LIBVLC_DLL% (
+  echo libvlc.dll not found in PATH
+  exit /b 1
+)
+
+set DEF_FILE="%THIS_BAT_PATH%libvlc.def"
+set DUMP_FILE="%THIS_BAT_PATH%libvlc.dump"
+
+if "%~1"=="" (
+  set LIB_FILE="%THIS_BAT_PATH%libvlc.lib"
+) else (
+  set LIB_FILE="%~1"
+)
+
+echo EXPORTS > %DEF_FILE%
+
+dumpbin /exports /out:%DUMP_FILE% %LIBVLC_DLL%
+
+for /f "usebackq tokens=4,* delims=_ " %%i in (%DUMP_FILE%) do if %%i==libvlc echo %%i_%%j >> %DEF_FILE%
+lib /NOLOGO /def:%DEF_FILE% /out:%LIB_FILE% /machine:x86
+del %DUMP_FILE% /f
+del %DEF_FILE% /f
